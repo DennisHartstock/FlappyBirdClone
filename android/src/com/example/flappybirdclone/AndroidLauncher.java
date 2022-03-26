5f14ea548a5ea6c44eb207500786579f8bcbf392
DennsHartstock <dennis.hartstock@web.de>
2022-03-26 05:03:22 +0100

    add background and bird rendering

--- android/src/com/example/flappybirdclone/AndroidLauncher.java
+++ android/src/com/example/flappybirdclone/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.example.flappybirdclone.Flappy Bird;
+import com.example.flappybirdclone.FlappyBird;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Flappy Bird(), config);
+		initialize(new FlappyBird(), config);
 	}
 }
