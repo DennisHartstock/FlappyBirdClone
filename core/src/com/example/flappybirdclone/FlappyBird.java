ee670e5f0a8e717fdd622a007ce8d84cd3380521
DennsHartstock <dennis.hartstock@web.de>
2022-03-27 08:47:23 +0200

    add bird falling

--- core/src/com/example/flappybirdclone/FlappyBird.java
+++ core/src/com/example/flappybirdclone/FlappyBird.java
@@ -11,6 +11,9 @@ public class FlappyBird extends ApplicationAdapter {
     Texture background;
     Texture[] bird;
     int birdStateFlag = 0;
+    float flyHeight;
+    float fallingSpeed = 0;
+    int gameStateFlag = 0;
 
     @Override
     public void create() {
@@ -19,10 +22,29 @@ public class FlappyBird extends ApplicationAdapter {
         bird = new Texture[2];
         bird[0] = new Texture("bird_wings_up.png");
         bird[1] = new Texture("bird_wings_down.png");
+        flyHeight = Gdx.graphics.getHeight() / 2 - bird[0].getHeight() / 2;
     }
 
     @Override
     public void render() {
+        if (Gdx.input.justTouched()) {
+            gameStateFlag = 1;
+        }
+
+        if (gameStateFlag == 1) {
+            if (Gdx.input.justTouched()) {
+                fallingSpeed = -30;
+            }
+            if (flyHeight > 0 || fallingSpeed < 0) {
+                fallingSpeed++;
+                flyHeight -= fallingSpeed;
+            }
+        } else {
+            if (Gdx.input.justTouched()) {
+                gameStateFlag = 1;
+            }
+        }
+
         if (birdStateFlag == 0) {
             birdStateFlag = 1;
         } else {
@@ -31,8 +53,8 @@ public class FlappyBird extends ApplicationAdapter {
 
         batch.begin();
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(bird[birdStateFlag], Gdx.graphics.getWidth() / 2 - bird[birdStateFlag].getWidth() / 2,
-                Gdx.graphics.getHeight() / 2 - bird[birdStateFlag].getHeight() / 2);
+        batch.draw(bird[birdStateFlag],
+                Gdx.graphics.getWidth() / 2 - bird[birdStateFlag].getWidth() / 2, flyHeight);
         batch.end();
     }
 }
